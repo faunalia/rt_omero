d906c8be0351d69b40c240cb9ddf7a8b2da4cbb6
giuseppe <giuseppe@4a317d5c-a739-48e9-9b60-9936a90f3597>
2012-05-19 00:49:39 +0000

    shrink the 'ID Scheda' value (follow r113)
    git-svn-id: https://trac.faunalia.it/svn/omero/trunk@114 4a317d5c-a739-48e9-9b60-9936a90f3597

--- SezPrincipale.py
+++ SezPrincipale.py
@@ -97,7 +97,7 @@ class SezPrincipale(QWidget, MappingPart, Ui_Form):
 <table class="blue">
 	<tr class="line">
 		<!--<td>ID Scheda</td><td class="value">%s<br><span class="tooltip">COD. ISTAT COMUNE</span></td><td class="value" width="5px">-</td><td class="value">%s<br><span class="tooltip">NUM. SCHEDA</span></td><td class="value" width="5px">_</td><td class="value">%s<br><span class="tooltip">ID_RIL</span></td>-->
-		<td>ID Scheda</td><td class="value center">%s<span style="padding: 0 5px;">-</span>%s<span style="padding: 0 5px;">_</span>%s<br><span class="tooltip">COD. ISTAT COMUNE<span style="padding: 0 10px;">-</span>NUM. SCHEDA<span style="padding: 0 10px;">_</span>ID_RIL</span></td>
+		<td>ID Scheda</td><td class="value center">%s-%s_%s<br><span class="tooltip">COD. ISTAT COMUNE<span style="padding: 0 10px;">-</span>NUM. SCHEDA<span style="padding: 0 10px;">_</span>ID_RIL</span></td>
 		<td class="line">Data</td><td class="value">%s<br><span class="tooltip">GG MM AAAA</span></td>
 	</tr>
 </table>
