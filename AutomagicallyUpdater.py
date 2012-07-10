45eb69948d643c18207aef0c7791bee195d13e86
Giuseppe Sucameli <brush.tyler@gmail.com>
2012-07-10 20:03:29 +0200

    disable debug mode (fix "IOError: [Errno 9] Bad file descriptor" on win)

--- AutomagicallyUpdater.py
+++ AutomagicallyUpdater.py
@@ -31,7 +31,7 @@ from ConnectionManager import ConnectionManager
 
 class AutomagicallyUpdater:
 
-	DEBUG = True
+	DEBUG = False
 
 	DEFAULT_CONN_TYPE = 1	# usa la connessione tramite pyspatialite
 	EDIT_CONN_TYPE = 1	# usa la connessione tramite pyspatialite
